cf57347a99af6207472b3675d56adc78f7b4fdec
enzo.tessier <enzo.tessier72@gmail.com>
2024-01-12 10:26:26 +0100

    page bouquet

---
+++ VivaFleurs/src/js/main.js
@@ -0,0 +1,19 @@
+
+
+
+
+document.getElementById(`image-fleur-1`).addEventListener("click", () => {
+    document.getElementById('principal').src = document.getElementById(`image-fleur-1`).src;
+});
+
+document.getElementById(`image-fleur-2`).addEventListener("click", () => {
+    document.getElementById('principal').src = document.getElementById(`image-fleur-2`).src;
+});
+
+document.getElementById(`image-fleur-3`).addEventListener("click", () => {
+    document.getElementById('principal').src = document.getElementById(`image-fleur-3`).src;
+});
+
+document.getElementById(`image-fleur-4`).addEventListener("click", () => {
+    document.getElementById('principal').src = document.getElementById(`image-fleur-4`).src;
+});
